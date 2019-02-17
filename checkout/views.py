3f7ee63f7db190c1c5b2db92551fb8d45af1be79
Thomas Dodson <tw.dodson@yahoo.co.uk>
2019-02-17 22:31:59 +0000

    prepping for heroku backup

--- checkout/views.py
+++ checkout/views.py
@@ -18,7 +18,7 @@ def checkout(request):
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
         
-        if order_form._is_valid() and payment_form.is_valid():
+        if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
             order.save()
